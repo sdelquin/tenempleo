da3e8a039ea5e7b154c93664c2d692e0e29150e6
Sergio Delgado Quintero <sdelquin@gmail.com>
2022-12-08 12:30:01 +0000

    Fix production setup

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ~/.virtualenvs/tenempleo/bin/activate
+source ~/.pyenv/versions/tenempleo/bin/activate
 cd "$(dirname "$0")"
 git pull
 pip install -r requirements.txt
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source ~/.virtualenvs/tenempleo/bin/activate
+source ~/.pyenv/versions/tenempleo/bin/activate
 cd "$(dirname "$0")"
 exec python main.py $@
